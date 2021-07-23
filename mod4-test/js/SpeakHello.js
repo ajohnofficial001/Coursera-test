61e949a027503485afac74d4fb5ebf651b1d1816
John Adeyemo <85674968+ajohnofficial001@users.noreply.github.com>
2021-07-23 23:14:24 +0100

    Add files via upload

---
+++ mod4-test/js/SpeakHello.js
@@ -0,0 +1,14 @@
+
+
+(function ( window){
+    var helloSpeaker = {};
+	var speakWord = "Hello";
+	helloSpeaker.speak = function (name) {
+	  console.log(speakWord + " " + name);
+}
+
+    window.helloSpeaker = helloSpeaker;
+
+})(window);
+
+
